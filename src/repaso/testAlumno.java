79d9d56715b0f1dae3eb080d8dd069039d127f27
Agustinicolas <agustinicolas@outlook.com>
2022-03-17 19:01:55 -0300

    agregados clase Alumno y testAlumno

---
+++ src/repaso/testAlumno.java
@@ -0,0 +1,23 @@
+package repaso;
+
+public class testAlumno {
+    
+    public static void main(String[] args){
+
+        Alumno a = new Alumno("FAI-222", "DNI", 25986544);
+        Alumno b = new Alumno("FAI-3434", "DNI", 43989898);
+
+        System.out.println("ALUMNO A");
+        System.out.println(a.aCadena());
+
+        b.setNombre("agustin");
+        b.setApellido("hernandez");
+        b.setDomicilioCalle("Rio Quinto");
+        b.setDomicilioNum(484);
+        b.setDomicilioCiudad("Neuquen");
+        b.setTelefono(2995216105L);
+        System.out.println("ALUMNO B");
+        System.out.println(b.aCadena());
+    }
+
+}
