1c35114df927b22fca9b810253fa64d9c8203b40
Agustin Hernandez <agustinicolas@outlook.com>
2022-03-25 11:31:48 -0300

    Implementada clase Nodo

---
+++ src/lineales/dinamicas/Nodo.java
@@ -0,0 +1,38 @@
+package lineales.dinamicas;
+
+public class Nodo {
+    
+    //ATRIBUTOS
+    private Object elem;
+    private Nodo enlace;
+
+
+    //CONSTRUCTOR
+    
+    public Nodo(Object elem, Nodo enlace){
+        this.elem = elem;
+        this.enlace = enlace;
+    }
+
+
+    //MODIFICADORES
+
+    public void setElem(Object elem){
+        this.elem = elem;
+    }
+
+    public void setEnlace(Nodo enlace){
+        this.enlace = enlace;
+    }
+
+
+    //OBSERVADORES
+
+    public Object getElem(){
+        return this.elem;
+    }
+
+    public Nodo getEnlace(){
+        return this.enlace;
+    }
+}
