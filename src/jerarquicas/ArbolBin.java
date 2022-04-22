a31e85b957c07dd40e0d5510c902b83433e97abe
Agustinicolas <agustinicolas@outlook.com>
2022-04-22 01:13:22 -0300

    implementacion arbol binario

---
+++ src/jerarquicas/ArbolBin.java
@@ -0,0 +1,338 @@
+package jerarquicas;
+
+import lineales.dinamicas.*;
+
+public class ArbolBin {
+    
+    private NodoArbol raiz;
+
+
+    //CONSTRUCTOR
+    public ArbolBin(){
+        this.raiz = null;
+    }
+
+
+    //METODOS PROPIOS
+
+    public boolean insertar (Object elemNuevo, Object elemPadre, char lugar){
+        /*  
+            inserta elemNuevo  como hijo del primer nodo encontrado en preorden igual a elemPadre, 
+            como hijo izquierdo (I) o hijo derecho (D), segun lo indique el parametro lugar
+        */  
+        boolean exito = true;
+
+        if(this.raiz == null){
+            //si el arbol esta vacio, pone elemNuevo en la raiz
+            this.raiz = new NodoArbol(elemNuevo, null, null);
+        }else{
+            //si arbol no esta vacio, busca el padre
+            NodoArbol nPadre = obtenerNodo(this.raiz, elemPadre);
+
+            //si padre existe y lugar no esta ocupado lo pone, sino da error
+            if(nPadre != null){
+                if (lugar == 'I' && nPadre.getIzquierdo() == null){
+                    nPadre.setIzquierdo(new NodoArbol(elemNuevo, null,null));
+                }else if(lugar == 'D' && nPadre.getDerecho() == null){
+                    nPadre.setDerecho(new NodoArbol(elemNuevo, null, null));
+                }else{
+                    exito = false;
+                }
+            }else{
+                exito = false;
+            }
+        }
+        return exito;
+    }
+
+
+    private NodoArbol obtenerNodo(NodoArbol n, Object buscado){
+        // metodo privado que busca un elemento y devuelve el nodo que
+        //lo contiene. Si no se encuentra el buscado devuelve null
+        NodoArbol resultado = null;
+
+        if (n != null){
+            if (n.getElem().equals(buscado)){
+                //si el buscado es n, lo devuelve
+                resultado = n;
+            }else{
+                //no es el buscado: busca primero en el HI
+                resultado = obtenerNodo(n.getIzquierdo(), buscado);
+                //si no lo encontro en el HI, busca en el HD
+                if (resultado == null){
+                    resultado = obtenerNodo(n.getDerecho(), buscado);
+                }
+            }
+        }
+        return resultado;
+    }
+
+
+    public boolean esVacio(){
+        boolean vacio = false;
+
+        if (this.raiz == null){
+            vacio = true;
+        }
+        return vacio;
+    }
+
+
+    public Object padre(Object elemento){
+        //Dado un elemento, devuelve el valor almacenado en su nodo padre (busca la primera aparicion del elemento)
+        NodoArbol padre = null;
+        Object elem = null;
+        if(!(this.esVacio() || elemento.equals(this.raiz.getElem()))){
+            padre = padreAux(elemento, this.raiz);
+        }
+
+        if (padre == null)
+            elem = null;
+        else
+            elem = padre.getElem();
+
+        return elem;
+    }
+
+
+    private NodoArbol padreAux(Object elemento, NodoArbol n){
+        //Metodo privado auxiliar del metodo padre
+        NodoArbol padre = null;
+
+        if (n != null){
+            if (  (n.getIzquierdo() != null && n.getIzquierdo().getElem().equals(elemento)) || (n.getDerecho() != null && n.getDerecho().getElem().equals(elemento)) ){
+                padre = n;
+            }else{
+                padre = padreAux(elemento, n.getIzquierdo());
+                if ( padre == null){
+                    padre = padreAux(elemento, n.getDerecho());
+                }
+            }
+        }
+        return padre;
+    }
+
+
+    public int altura(){
+        //Devuelve la altura del arbol, es decir la longitud del camino más largo desde la raíz hasta una hoja
+        //Un arbol vacío tiene altura -1, una hoja tiene altura 0
+        int altura = -1;
+
+        if (!this.esVacio()){
+            altura = 0;
+            altura = alturaAux(this.raiz);
+        }
+        return altura;
+    }
+
+
+    private int alturaAux(NodoArbol n){
+        int altura = -1;
+        int alturaIzq;
+        int alturaDer;
+        if (n != null){
+            alturaIzq = alturaAux(n.getIzquierdo());
+            alturaDer = alturaAux(n.getDerecho());
+            altura = mayor(alturaIzq, alturaDer)+1;
+        }
+        return altura;
+    }
+
+    private int mayor(int a, int b){
+        //metodo privado auxiliar a altura aux, dados dos enteros ingresados por parametro devuelve el mayor.
+
+        int mayor = a;
+        if (b > a){
+            mayor = b;
+        }
+        return mayor;
+    }
+
+
+    public int nivel(Object elem){
+        //Devuelve el nivel de un elemento del arbol. Si el elemento no existe el arbol devuelve -1
+        int resultado = -1;
+        
+        if (!this.esVacio()){
+            resultado = nivelAux(this.raiz, elem, -1,);
+        }
+        return resultado;
+    }
+
+
+    private int nivelAux(NodoArbol n, Object elem, int nivel){ //TENGO QUE DETENER EJECUCION UNA VEZ QUE ENCUENTRA EL ELEMENTO!!!!
+        //metodo privado auxiliar al metodo nivel
+        int resul = -1;
+
+        if(n != null){
+            if ( n.getElem().equals(elem)){
+                resul = nivel+1;
+            }else{
+                resul = nivelAux(n.getIzquierdo(), elem, nivel+1);
+                if (resul == -1){
+                    resul = nivelAux(n.getDerecho(), elem, nivel+1);
+                }
+            }
+        }
+
+        return resul;
+    }
+
+
+    public Lista listarPreOrden(){
+        //retorna una lista con los elementos del arbol en PREORDEN
+        Lista lis = new Lista();
+        listarPreOrdenAux(this.raiz, lis);
+        return lis;
+    }
+
+
+    private void listarPreOrdenAux(NodoArbol nodo, Lista lis){
+        //metodo recursivo privado porque su parametro es del tipo NodoArbol
+
+        if(nodo != null){
+            //visita el elemento del nodo
+            lis.insertar(nodo.getElem(), lis.longitud()+1); //(1)
+
+            //recorre a sus hijos en preorden
+            listarPreOrdenAux(nodo.getIzquierdo(), lis); //(2)
+            listarPreOrdenAux(nodo.getDerecho(), lis); //(3)
+        }
+    }
+
+
+    public Lista listarInOrden(){
+        //retorna una lista con los elementos del arbol en INORDEN
+        Lista lis = new Lista();
+        listarInOrdenAux(this.raiz, lis);
+        return lis;
+    }
+
+
+    private void listarInOrdenAux(NodoArbol nodo, Lista lis){
+        //metodo recursivo privado porque su parametro es del tipo NodoArbol
+
+        if(nodo != null){
+            //recorre el hijo izquierdo en inorden
+            listarInOrdenAux(nodo.getIzquierdo(), lis); //(1)
+            //visita la raiz del subarbol
+            lis.insertar(nodo.getElem(), lis.longitud()+1); //(2)
+            //recorre el hijo derecho en inorden
+            listarPreOrdenAux(nodo.getDerecho(), lis); //(3)
+        }
+    }
+
+
+    public Lista listarPosOrden(){
+        //retorna una lista con los elementos del arbol en POSORDEN
+        Lista lis = new Lista();
+        listarPosOrdenAux(this.raiz, lis);
+        return lis;
+    }
+
+
+    private void listarPosOrdenAux(NodoArbol nodo, Lista lis){
+        //metodo recursivo privado porque su parametro es del tipo NodoArbol
+
+        if(nodo != null){
+            //recorre a sus hijos en posorden
+            listarPosOrdenAux(nodo.getIzquierdo(), lis); //(1)
+            listarPosOrdenAux(nodo.getDerecho(), lis); //(2)
+            //visita el elemento del nodo
+            lis.insertar(nodo.getElem(), lis.longitud()+1); //(3)
+        }
+    }
+
+
+    public Lista listarNiveles(){
+        //Devuelve una lista con los elementos del arbol binario en recorrido por niveles
+        Cola cola = new Cola();
+        Lista lis = new Lista();
+        NodoArbol nodoActual;
+        if (!this.esVacio()){
+            cola.poner(this.raiz);
+            while ( !cola.esVacia()){
+                nodoActual = (NodoArbol) cola.obtenerFrente();
+                cola.sacar();
+                lis.insertar(nodoActual.getElem(), lis.longitud()+1);
+                if ( nodoActual.getIzquierdo() != null){
+                    cola.poner(nodoActual.getIzquierdo());
+                }
+                if(nodoActual.getDerecho() != null){
+                    cola.poner(nodoActual.getDerecho());
+                }
+            }
+        }
+        return lis;
+
+    }
+
+
+    public void vaciar(){
+        this.raiz = null;
+    }
+
+    
+    @Override
+    public ArbolBin clone(){
+        //Genera y crea un arbol binario equivalente al original
+
+        ArbolBin arbolClon = new ArbolBin();
+
+        if(!this.esVacio()){
+            arbolClon.raiz = cloneAux(this.raiz);
+        }
+
+        return arbolClon;
+    }
+
+
+    private NodoArbol cloneAux(NodoArbol n){
+        //metodo privado auxiliar al metodo clone
+        
+        NodoArbol nodoNuevo = new NodoArbol(null,null,null);
+        if (n != null){
+            nodoNuevo.setElem(n.getElem());
+            if(n.getIzquierdo() != null)
+                nodoNuevo.setIzquierdo(cloneAux(n.getIzquierdo()));
+            if(n.getDerecho() != null)
+                nodoNuevo.setDerecho(cloneAux(n.getDerecho()));
+        }
+        return nodoNuevo;
+    }
+
+
+    @Override
+    public String toString(){
+        //Genera y devuelve una cadena de caracteres que indica cual es la raiz del arbol
+        //y quienes son los hijos de cada nodo
+        String cadena = "";
+        if (this.esVacio()){
+            cadena = "Arbol vacio.";
+        }else{
+            cadena = toStringAux(this.raiz);
+        }
+        return cadena;
+    }
+
+
+    private String toStringAux(NodoArbol n){
+        //metodo privado auxiliar al metodo toString
+        String cadena = "";
+        String hi = " - ";
+        String hd = " - ";
+        if(n!= null){
+            cadena = "\nElem: "+ n.getElem().toString(); // impresión del nodo visitado
+            if (n.getIzquierdo() != null)
+                hi = n.getIzquierdo().getElem().toString();
+            if (n.getDerecho() != null)
+                hd = n.getDerecho().getElem().toString();
+            cadena = cadena + "\t HI: "+hi+"\t HD: "+hd;
+            cadena = cadena + toStringAux(n.getIzquierdo()); //recorre subarbol izquierdo del nodo
+            cadena = cadena + toStringAux(n.getDerecho()); //recorre subarbol derecho del nodo
+            
+        }
+        return cadena;
+    }
+
+}
---
+++ src/tests/jerarquicas/TestArbolBinPropio.java
@@ -0,0 +1,91 @@
+package tests.jerarquicas;
+
+import jerarquicas.ArbolBin;
+import lineales.dinamicas.*;
+
+public class TestArbolBinPropio {
+    
+    public static void main(String[] args){
+        
+        ArbolBin arbol1 = new ArbolBin();
+        ArbolBin arbol2;
+        Lista listaPre = new Lista();    
+        Lista listaPos = new Lista(); 
+        Lista listaIn = new Lista();
+        Lista listaNiv = new Lista();     
+        System.out.println("IMPRESION ARBOL VACIO, ESPERA ARBOL VACIO --- " + arbol1.toString()); 
+        arbol2 = arbol1.clone();
+        System.out.println("IMPRESION ARBOL CLON VACIO, ESPERA ARBOL VACIO --- " + arbol2.toString()); 
+        System.out.println("METODO ESVACIO EN ARBOL VACIO, ESPERA TRUE --- "+arbol1.esVacio()); 
+        System.out.println("METODO ALTURA EN ARBOL VACIO, ESPERA -1 --- "+ arbol1.altura());
+        System.out.println("METODO NIVEL EN ELEMENTO INEXISTENTE, ESPERA -1 --- " + arbol1.nivel("ASD"));
+        System.out.println("METODO NIVEL EN ELEMENTO 'A', ESPERA -1 --- " + arbol1.nivel('A'));
+        boolean exito;
+        exito = arbol1.insertar('A', "asd", 'D'); //INSERTO ELEMENTO
+        System.out.println("\nInsercion: "+exito + "\n"+ arbol1.toString()+"\n"); //IMPRIMO ARBOL
+        System.out.println("IMPRESION ARBOL CLON, ESPERA ARBOL VACIO --- " + arbol2.toString());        
+        System.out.println("METODO ESVACIO EN ARBOL  NO VACIO, ESPERA FALSE --- "+(arbol1.esVacio())); //METODO ESVACIO EN ARBOL NO VACIO
+        exito = arbol1.insertar('B','Z','I');
+        System.out.println("INTENTO INSERTAR NODO EN PADRE INEXISTENTE, ESPERA FALSE --- "+exito);
+        System.out.println("\n"+arbol1.toString()+"\n");
+        System.out.println("METODO NIVEL EN ELEMENTO INEXISTENTE, ESPERA -1 --- " + arbol1.nivel("ASD"));
+        System.out.println("METODO NIVEL EN ELEMENTO 'A', ESPERA 0 --- " + arbol1.nivel('A'));
+        System.out.println("METODO ALTURA EN ARBOL, ESPERA 0 --- "+ arbol1.altura());
+        System.out.println("METODO PADRE EN RAIZ, ESPERA NULL-TRUE --- " + (arbol1.padre('A') == null));
+
+        exito = arbol1.insertar('B', 'A', 'I');
+        System.out.println("\nInsercion: "+exito + "\n"+ arbol1.toString()+"\n"); //INSERTO B HIJO IZQUIERDO A, ESPERO TRUE
+
+        exito = arbol1.insertar('C', 'A', 'I');
+        System.out.println("\nInsercion: "+exito + "\n"+ arbol1.toString()+"\n"); //INSERTO C HIJO IZQUIERDO A, ESPERO FALSE
+
+        exito = arbol1.insertar('C', 'A', 'D');
+        System.out.println("\nInsercion: "+exito + "\n"+ arbol1.toString()+"\n"); //IMPRIMO C HIJO DERECHO A, ESPERO TRUE
+
+        System.out.println("METODO NIVEL EN ELEMENTO 'C', ESPERA 1 --- " + arbol1.nivel('C'));
+        System.out.println("METODO ALTURA EN ARBOL, ESPERA 1 --- "+ arbol1.altura());
+
+        exito = arbol1.insertar('D', 'C', 'D');
+        System.out.println("\nInsercion: "+exito + "\n"+ arbol1.toString()+"\n"); //IMPRIMO D HIJO DERECHO C, ESPERO TRUE
+        System.out.println("METODO NIVEL EN ELEMENTO INEXISTENTE, ESPERA -1 --- " + arbol1.nivel("ASD"));
+        System.out.println("METODO NIVEL EN ELEMENTO 'D', ESPERA 2 --- " + arbol1.nivel('D'));
+        System.out.println("METODO ALTURA EN ARBOL, ESPERA 2 --- "+ arbol1.altura());
+        System.out.println("METODO PADRE EN RAIZ, ESPERA NULL-TRUE --- " + (arbol1.padre('A') == null));
+        System.out.println("METODO PADRE EN 'D', ESPERA C --- " + arbol1.padre('D'));
+        System.out.println("METODO PADRE EN 'C', ESPERA A --- " + arbol1.padre('C'));
+
+        listaPre = arbol1.listarPreOrden();
+        listaPos = arbol1.listarPosOrden();
+        listaIn = arbol1.listarInOrden();
+        listaNiv = arbol1.listarNiveles();
+
+        System.out.println("\n LISTA PREORDEN:\n" + listaPre.toString() );
+        System.out.println("\n LISTA POSORDEN:\n" + listaPos.toString());
+        System.out.println("\n LISTA INORDEN:\n"+listaIn.toString());        
+        System.out.println("\n LISTA POR NIVELES:\n"+listaNiv.toString());
+
+        arbol2 = arbol1.clone();
+
+        System.out.println("IMPRESION ARBOL ORIGINAL:\n"+arbol1.toString());
+        System.out.println("IMPRESION ARBOL CLON:\n" +arbol2.toString());
+
+        arbol1.vaciar();
+
+        System.out.println("IMPRESION ARBOL ORIGINAL VACIO:\n"+arbol1.toString());
+        System.out.println("IMPRESION ARBOL CLON:\n" +arbol2.toString());
+
+        arbol2.insertar('E', 'B', 'I');
+        arbol2.insertar('F','C','I');
+        arbol2.insertar('G','F','I');
+        arbol2.insertar('H','F','D');
+
+        System.out.println("IMPRESION ARBOL CLON:\n" +arbol2.toString());
+        System.out.println("METODO ESVACIO EN ARBOL VACIO, ESPERA TRUE --- "+arbol1.esVacio()); 
+        System.out.println("METODO ALTURA EN ARBOL CLON, ESPERA 3 --- "+ arbol2.altura());
+        System.out.println("METODO NIVEL EN ELEMENTO INEXISTENTE, ESPERA -1 --- " + arbol1.nivel("ASD"));
+        System.out.println("METODO NIVEL EN ELEMENTO 'G', ESPERA 3 --- " + arbol1.nivel('G'));
+        System.out.println("METODO PADRE EN ELEMENTO INEXISTENTE, ESPERA NULL/true --- " +(arbol2.padre('K'))==null);
+        System.out.println("METODO PADRE EN ELEMENTO G, ESPERA F --- " +arbol2.padre('G'));
+        
+    }
+}
