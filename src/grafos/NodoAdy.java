470a51da300b5dd3788bd7ae9b57681c0b4e076f
Agustinicolas <agustinicolas@outlook.com>
2025-03-18 12:16:04 -0300

    Implementados atributos y comportamiento basico NodoVert y NodoAdy

--- src/grafos/NodoAdy.java
+++ src/grafos/NodoAdy.java
@@ -2,4 +2,26 @@ package grafos;
 
 public class NodoAdy {
 
+    //atributos
+    private NodoVert vertice;
+    private NodoAdy sigAdyacente;
+
+
+    public NodoVert getVertice(){
+        return this.vertice;
+    }
+
+    public void setVertice(NodoVert nuevoVert){
+        this.vertice = nuevoVert;
+    }
+
+    public NodoAdy getSigAdyacente(){
+        return this.sigAdyacente;
+    }
+
+    public void setSigAdyacente(NodoAdy nuevoAdy){
+        this.sigAdyacente = nuevoAdy;
+    }
+
+
 }
--- src/grafos/NodoVert.java
+++ src/grafos/NodoVert.java
@@ -1,5 +1,33 @@
 package grafos;
 
 public class NodoVert {
+    
+    //atributos
+    private Object elem;
+    private NodoVert sigVertice;
+    private NodoAdy primerAdy;
 
+    public Object getElem(){
+        return this.elem;
+    }
+
+    public void setElem(Object elemNuevo){
+        this.elem = elemNuevo;
+    }
+
+    public Object getSigVertice(){
+        return primerAdy.getVertice();
+    }
+
+    public void setSigVert(NodoVert nuevoVert){
+        this.sigVertice = nuevoVert;
+    }
+
+    public NodoAdy getPrimerAdy(){
+        return this.primerAdy;
+    }
+
+    public void setPrimerAdy(NodoAdy nuevoAdy){
+        this.primerAdy = nuevoAdy;
+    }
 }
